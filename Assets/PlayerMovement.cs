047d8360d092bd4be15a22c54874b9e6f2ad036d
Codidodido <codidodido@gmail.com>
2023-08-29 23:56:53 +0330

    Implement Player running with animations

--- Assets/PlayerMovement.cs
+++ Assets/PlayerMovement.cs
@@ -3,9 +3,13 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
+
 public class PlayerMovement : MonoBehaviour
 {
+    [Header("Player")]
     [SerializeField] private float moveSpeed;
+
+    private float _extraMoveSpeed;
     private Rigidbody2D _rigidbody2D;
     private Vector2 _movement;
     private Animator _animator;
@@ -13,6 +17,8 @@ public class PlayerMovement : MonoBehaviour
     {
         _rigidbody2D = GetComponent<Rigidbody2D>();
         _animator = GetComponent<Animator>();
+
+        _extraMoveSpeed = 0f;
     }
     
     private void Update()
@@ -23,10 +29,26 @@ public class PlayerMovement : MonoBehaviour
         _animator.SetFloat("Horizontal",_movement.x);
         _animator.SetFloat("Vertical",_movement.y);
         _animator.SetFloat("Speed",_movement.sqrMagnitude);
+        CheckRunning();
     }
 
     private void FixedUpdate()
     {
-        _rigidbody2D.MovePosition(_rigidbody2D.position + _movement * (moveSpeed * Time.fixedDeltaTime));
+        _rigidbody2D.MovePosition(_rigidbody2D.position + _movement * ((moveSpeed+_extraMoveSpeed) * Time.fixedDeltaTime));
     }
+
+    private void CheckRunning()
+    {
+        if (Input.GetKey(KeyCode.LeftShift))
+        {
+            _animator.SetBool("IsRunning", true); 
+            _extraMoveSpeed = 0.5f;
+             
+        }else 
+        {
+            _animator.SetBool("IsRunning", false);
+            _extraMoveSpeed = 0f; 
+        }
+    }
+    
 }
