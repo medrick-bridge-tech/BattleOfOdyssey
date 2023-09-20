413f476d942aca0811f16f1de48da589de5425f1
Codidodido <codidodido@gmail.com>
2023-09-20 19:17:43 +0330

    Create UICoin script

---
+++ Assets/Scripts/GameManager.cs
@@ -0,0 +1,10 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameManager : MonoBehaviour
+{ 
+    public static int Coins { get; private set; }
+    public static int Kills { get; private set; }
+    
+}
---
+++ Assets/Scripts/UI/UICoin.cs
@@ -0,0 +1,21 @@
+using System;
+using TMPro;
+using UnityEngine;
+
+namespace UI
+{
+    public class UICoin : MonoBehaviour
+    {
+        [SerializeField] private TextMeshProUGUI coinText;
+
+        private void Update()
+        {
+            SetCoin();
+        }
+
+        private void SetCoin()
+        {
+            coinText.text = $": {GameManager.Coins}";
+        }
+    }
+}
