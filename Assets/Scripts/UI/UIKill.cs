efc87e22b96bf49930829aebb52abe5051a8f3da
Codidodido <codidodido@gmail.com>
2023-09-20 19:28:15 +0330

    Create UIKill script

---
+++ Assets/Scripts/UI/UIKill.cs
@@ -0,0 +1,20 @@
+using TMPro;
+using UnityEngine;
+
+namespace UI
+{
+    public class UIKill : MonoBehaviour
+    {
+        [SerializeField] private TextMeshProUGUI killText;
+
+        private void Update()
+        {
+            SetKill();
+        }
+
+        private void SetKill()
+        {
+            killText.text = $": {GameManager.Kills}";
+        }
+    }
+}
