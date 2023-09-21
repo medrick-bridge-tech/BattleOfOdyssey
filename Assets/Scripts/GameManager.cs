a1616ef8edbee8d6f987548e71e9371899cd4490
Codidodido <codidodido@gmail.com>
2023-09-22 00:38:43 +0330

    Create UIMessage script

--- Assets/Scripts/GameManager.cs
+++ Assets/Scripts/GameManager.cs
@@ -1,10 +1,18 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
+using UI;
 using UnityEngine;
 
 public class GameManager : MonoBehaviour
 { 
     public static int Coins { get; private set; }
     public static int Kills { get; private set; }
-    
+
+    [SerializeField] private UIMessage uiMessage;
+
+    private void Start()
+    {
+        uiMessage.UpdateMessage("Kill all the factory guards to open the door.");
+    }
 }
